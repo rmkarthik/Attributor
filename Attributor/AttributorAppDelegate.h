81eb05f9b11adc01a13d7c46b3532fdf75acced2
Karthik <karthik@Mobilitys-MacBook-Pro-2.local>
2014-02-13 18:27:33 +0530

    Attributor is being checked in

---
+++ Attributor/AttributorAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AttributorAppDelegate.h
+//  Attributor
+//
+//  Created by Karthik on 2/13/14.
+//  Copyright (c) 2014 Karthik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AttributorAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Attributor/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Attributor
+//
+//  Created by Karthik on 2/13/14.
+//  Copyright (c) 2014 Karthik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AttributorAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AttributorAppDelegate class]));
+    }
+}
